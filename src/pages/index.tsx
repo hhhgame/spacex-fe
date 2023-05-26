6f974bb073bb15fc6a9339425abadb644a5b67fd
wangli <wx_685ca221199f434cbd58ee265d03b30e@git.code.tencent.com>
2023-05-27 00:55:19 +0800

    feat: spacex list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import {useRequest} from "ahooks";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -16,12 +16,83 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 import LoadingButton from '@mui/lab/LoadingButton';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import dayjs from "dayjs";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { green, orange } from '@mui/material/colors';
+import Fade from '@mui/material/Fade';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
 const getLanches = async (data) => {
   return axios.post('https://api.spacexdata.com/v5/launches/query', {
     options: data
   })
 }
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "transparent",
+          color: "#fff",
+          border: '1px solid #444744'
+        }
+      }
+    }
+  }
+  // palette: {
+  //   mode: 'dark',
+  //   primary: {
+  //     main: '#ffffff',
+  //     b
+  //   },
+  // },
+});
 
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window;
+  children: React.ReactElement;
+}
+
+function ScrollTop(props: Props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    const anchor = (
+      (event.target as HTMLDivElement).ownerDocument || document
+    ).querySelector('#back-to-top-anchor');
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        block: 'center',
+      });
+    }
+  };
+
+  return (
+    <Fade in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: 'fixed', bottom: 16, right: 16 }}
+      >
+        {children}
+      </Box>
+    </Fade>
+  );
+}
 export default function HomePage() {
   const [pageInfo, setPageInfo] = useState<any>({
     page: 1,
@@ -54,9 +125,15 @@ export default function HomePage() {
         totalPages: data.data.totalPages,
         nextPage: data.data.nextPage,
       })
-    }
+    }                                             ``
   }, [data])
+
+  function gotoDetail() {
+    console.warn('detail')
+  }
+
   return (
+    <ThemeProvider theme={darkTheme}>
     <Box sx={{mt:5}} style={{width: '1200px', margin: '0 auto'}}>
       <CssBaseline />
       <Container maxWidth="m" >
@@ -70,28 +147,39 @@ export default function HomePage() {
             {
               listData.length > 0 ? listData.map(item => {
                 return (
-                  <ListItem disablePadding>
-                    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                  <ListItem key={item.date_utc} disablePadding>
+                    <React.Fragment key={item.id}>
+                      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
                       {
                         item.map((innerItem, index) => (
-                          <Grid xs={6}>
-                            <Box>
+                          <React.Fragment key={innerItem.id}> <Grid xs={6} >
+                            <Box
+                              sx={{
+                                marginBottom: '10px',
+                                border: '1px solid #444744',
+                                marginRight: 10,
+                                padding: '20px',
+                                borderRadius: '15px'
+                              }}
+                            >
                               <LazyLoadImage
                                 effect="blur"
                                 width={'100%'}
                                 // height={image.height}
                                 src={item[index]?.links.patch.large} // use normal <img> attributes as props
                                 // width={image.width} />
-                                />
+                              />
                               {/*<img style={{display: 'block', width: '100%'}} src={item[index]?.links.patch.large} />*/}
                               <p>{dayjs(item[index].date_utc).format('MMMM DD, YYYY ')}</p>
                               <p>{item[index].name}</p>
-                              <Button>detail</Button>
+                              <Button onClick={gotoDetail}>detail</Button>
                             </Box>
-                          </Grid>
+                          </Grid></React.Fragment>
+
                         ))
                       }
                     </Grid>
+                    </React.Fragment>
                   </ListItem>
                 )
               }) : <span>empty</span>
@@ -104,5 +192,6 @@ export default function HomePage() {
 
       </Container>
     </Box>
+    </ThemeProvider>
   )
 }
